5b55d7a14521669f9f8417d1bd18bd143aa55c06
yakoubsmail <100692487+yakoubsmail@users.noreply.github.com>
2022-03-21 20:26:23 +0100

    MON COMMIT

--- webserv.py
+++ webserv.py
@@ -1,3 +1,4 @@
+from shodan import Shodan
 from typing import Optional
 from fastapi import FastAPI
 
@@ -6,3 +7,20 @@ app = FastAPI()
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
+   
+
+@app.get("/ip/{ip}")
+async def get_ip(ip: str, key: Optional[str] = None):
+    if key is None:
+        return {"Error": "Please provide a valid API key"}
+    else:
+        try:
+            api = Shodan(key)
+            res = api.host(ip)
+            return {
+                "IP": res["ip_str"],
+                "Organization": res["org"],
+                "Country": res["country_name"],
+            }
+        except Exception as e:
+            return {"Error": str(e)}
